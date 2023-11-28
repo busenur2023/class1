e21acf904e60aeed96f09258b89620907d368885
busenur2023 <145591188+busenur2023@users.noreply.github.com>
2023-11-28 14:12:48 +0300

    Add files via upload
    ev bilgileri

---
+++ evbilgileri/evbilgileri/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace evbilgileri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMetre = new System.Windows.Forms.TextBox();
+            this.txtSalon = new System.Windows.Forms.TextBox();
+            this.txtÇocuko = new System.Windows.Forms.TextBox();
+            this.txtBanyo = new System.Windows.Forms.TextBox();
+            this.txtYaş = new System.Windows.Forms.TextBox();
+            this.txtKonum = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtKat = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(49, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Konum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(49, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "metre kare";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(46, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salon sayısı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(46, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Çocuk odası s.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(49, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Banyo sayısı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(49, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Binanın yaşı";
+            // 
+            // txtMetre
+            // 
+            this.txtMetre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMetre.Location = new System.Drawing.Point(179, 75);
+            this.txtMetre.Name = "txtMetre";
+            this.txtMetre.Size = new System.Drawing.Size(166, 26);
+            this.txtMetre.TabIndex = 7;
+            // 
+            // txtSalon
+            // 
+            this.txtSalon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSalon.Location = new System.Drawing.Point(179, 116);
+            this.txtSalon.Name = "txtSalon";
+            this.txtSalon.Size = new System.Drawing.Size(166, 26);
+            this.txtSalon.TabIndex = 8;
+            // 
+            // txtÇocuko
+            // 
+            this.txtÇocuko.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtÇocuko.Location = new System.Drawing.Point(179, 154);
+            this.txtÇocuko.Name = "txtÇocuko";
+            this.txtÇocuko.Size = new System.Drawing.Size(166, 26);
+            this.txtÇocuko.TabIndex = 9;
+            // 
+            // txtBanyo
+            // 
+            this.txtBanyo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBanyo.Location = new System.Drawing.Point(179, 194);
+            this.txtBanyo.Name = "txtBanyo";
+            this.txtBanyo.Size = new System.Drawing.Size(166, 26);
+            this.txtBanyo.TabIndex = 10;
+            // 
+            // txtYaş
+            // 
+            this.txtYaş.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYaş.Location = new System.Drawing.Point(179, 231);
+            this.txtYaş.Name = "txtYaş";
+            this.txtYaş.Size = new System.Drawing.Size(166, 26);
+            this.txtYaş.TabIndex = 11;
+            // 
+            // txtKonum
+            // 
+            this.txtKonum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKonum.Location = new System.Drawing.Point(179, 37);
+            this.txtKonum.Name = "txtKonum";
+            this.txtKonum.Size = new System.Drawing.Size(166, 26);
+            this.txtKonum.TabIndex = 13;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFiyat.Location = new System.Drawing.Point(179, 275);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(166, 26);
+            this.txtFiyat.TabIndex = 14;
+            this.txtFiyat.TextChanged += new System.EventHandler(this.txtFiyat_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(49, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Fiyat";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(46, 321);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Kat Sayısı";
+            // 
+            // txtKat
+            // 
+            this.txtKat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKat.Location = new System.Drawing.Point(175, 315);
+            this.txtKat.Name = "txtKat";
+            this.txtKat.Size = new System.Drawing.Size(166, 26);
+            this.txtKat.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(12, 367);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(155, 20);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Ebeveyin Banyosu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(175, 361);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 26);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(81, 410);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 20);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Asansör";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(175, 407);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 26);
+            this.textBox2.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(175, 450);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(166, 26);
+            this.textBox3.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(84, 458);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(58, 20);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Isıtma";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 515);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtKat);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.txtKonum);
+            this.Controls.Add(this.txtYaş);
+            this.Controls.Add(this.txtBanyo);
+            this.Controls.Add(this.txtÇocuko);
+            this.Controls.Add(this.txtSalon);
+            this.Controls.Add(this.txtMetre);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMetre;
+        private System.Windows.Forms.TextBox txtSalon;
+        private System.Windows.Forms.TextBox txtÇocuko;
+        private System.Windows.Forms.TextBox txtBanyo;
+        private System.Windows.Forms.TextBox txtYaş;
+        private System.Windows.Forms.TextBox txtKonum;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtKat;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ evbilgileri/evbilgileri/ilanBilgileri.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evbilgileri
+{
+    internal class ilanBilgileri
+    {
+        string konum;
+        string kulanimdurumu;
+        string ısıtma;
+        int salonsayisi;
+        int çocukodasi;
+        int banyosayisi;
+        int binayasi;
+        float fiyat;
+        bool ebeveyinbanyosu;
+        int katsayisi;
+        bool asansör;
+        bool balkonsayisi;
+
+        public string Konum { get => konum; set => konum = value; }
+        public string Kulanimdurumu { get => kulanimdurumu; set => kulanimdurumu = value; }
+        public string Isıtma { get => ısıtma; set => ısıtma = value; }
+        public int Salonsayisi { get => salonsayisi; set => salonsayisi = value; }
+        public int Çocukodasi { get => çocukodasi; set => çocukodasi = value; }
+        public int Banyosayisi { get => banyosayisi; set => banyosayisi = value; }
+        public int Binayasi { get => binayasi; set => binayasi = value; }
+        public float Fiyat { get => fiyat; set => fiyat = value; }
+        public bool Ebeveyinbanyosu { get => ebeveyinbanyosu; set => ebeveyinbanyosu = value; }
+        public int Katsayisi { get => katsayisi; set => katsayisi = value; }
+        public bool Asansör { get => asansör; set => asansör = value; }
+        public bool Balkonsayisi { get => balkonsayisi; set => balkonsayisi = value; }
+
+        public ilanBilgileri(string konum, string kulanimdurumu, string ısıtma, int salonsayisi, int çocukodasi, int banyosayisi, int binayasi, float fiyat, bool ebeveyinbanyosu, int katsayisi, bool asansör, bool balkonsayisi)
+        {
+            Konum = konum;
+            Kulanimdurumu = kulanimdurumu;
+            Isıtma = ısıtma;
+            Salonsayisi = salonsayisi;
+            Çocukodasi = çocukodasi;
+            Banyosayisi = banyosayisi;
+            Binayasi = binayasi;
+            Fiyat = fiyat;
+            Ebeveyinbanyosu = ebeveyinbanyosu;
+            Katsayisi = katsayisi;
+            Asansör = asansör;
+            Balkonsayisi = balkonsayisi;
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
